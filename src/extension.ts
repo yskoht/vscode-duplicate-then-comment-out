511341691477815c436df5098362584b440be372
yskoht <ysk.oht@gmail.com>
2022-05-15 14:19:10 +0900

    Insert blank line if next line position is eof

--- src/extension.ts
+++ src/extension.ts
@@ -28,6 +28,9 @@ const main = async () => {
 	const lsc = lineSeparatorCharacter(editor.document.eol);
 	const nextLinePosition = new vscode.Position(endLine.range.end.line + 1, 0);
 	editor.edit(editBuilder => {
+		if (editor.document.lineCount === nextLinePosition.line) {
+			editBuilder.insert(nextLinePosition, lsc);
+		}
 		editBuilder.insert(nextLinePosition, text + lsc);
 	});
 	editor.selection = new vscode.Selection(nextLinePosition, nextLinePosition);
